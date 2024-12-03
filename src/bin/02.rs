6eaa423df685de5d358de59b60387a44c2b45369
Abrar Habib <abrarhabib285@gmail.com>
2024-12-03 15:12:06 -0500

    Day 2: Red-Nosed Reports

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,25 +1,56 @@
 advent_of_code::solution!(2);
 
 fn parse(input: &str) -> Vec<u32> {
-    input.split_whitespace().filter_map(|s| s.parse::<u32>().ok()).collect()
+    input
+        .split_whitespace()
+        .filter_map(|s| s.parse::<u32>().ok())
+        .collect()
+}
+
+fn apply_safety_check<F>(safety_func: F, contents: &str) -> u32
+where
+    F: Fn(&Vec<u32>) -> bool,
+{
+    contents
+        .lines()
+        .map(|line| {
+            parse(line)
+        })
+        .filter(|report| safety_func(report))
+        .count() as u32
+}
+
+fn is_safe(report: &Vec<u32>) -> bool {
+    let is_desc = report[0] > report[1];
+    for i in 0..(report.len() - 1) {
+        let a = report[i];
+        let b = report[i + 1];
+        if is_desc && a < b || !is_desc && a > b {
+            return false;
+        }
+
+        let diff = a.abs_diff(b);
+        if !(1..=3).contains(&diff){
+            return false;
+        }
+    }
+    return true;
+}
+
+fn is_safe_with_one_removed(report: &Vec<u32>) -> bool {
+    (0..report.len()).any(|i| {
+        let mut tmp = report.clone();
+        tmp.remove(i);
+        is_safe(&tmp)
+    })
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let reports = input.lines().map(|line| parse(line)).collect::<Vec<Vec<u32>>>();
-    println!("{:?}", &reports);
-    let safe_count = reports.iter().fold(0, |acc, report| {
-        let is_valid = report.windows(2).all(|pair| {
-            let (first, second) = (pair[0], pair[1]);
-            let diff = first.abs_diff(second);
-            diff >= 1 && diff <= 3 && (first < second || first > second)
-        });
-        acc + if is_valid { 1 } else { 0 }
-    });
-    Some(safe_count)
+    Some(apply_safety_check(|report| is_safe(report), input))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(apply_safety_check(|report| is_safe_with_one_removed(report), input))
 }
 
 #[cfg(test)]
