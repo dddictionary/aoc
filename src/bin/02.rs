7fd61165236253f5f2a5e773b4d784b7223d9dce
Abrar Habib <abrarhabib285@gmail.com>
2024-12-03 01:04:44 -0500

    Day 2: Red-Nosed Reports P1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,7 +1,21 @@
 advent_of_code::solution!(2);
 
+fn parse(input: &str) -> Vec<u32> {
+    input.split_whitespace().filter_map(|s| s.parse::<u32>().ok()).collect()
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let reports = input.lines().map(|line| parse(line)).collect::<Vec<Vec<u32>>>();
+    println!("{:?}", &reports);
+    let safe_count = reports.iter().fold(0, |acc, report| {
+        let is_valid = report.windows(2).all(|pair| {
+            let (first, second) = (pair[0], pair[1]);
+            let diff = first.abs_diff(second);
+            diff >= 1 && diff <= 3 && (first < second || first > second)
+        });
+        acc + if is_valid { 1 } else { 0 }
+    });
+    Some(safe_count)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
